1852d31792eb9ff26deb64a1f073fd52770333ca
pw-02 <watters.patrick@outlook.com>
2024-09-01 16:09:35 -0700

    updated entrypoint for docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,6 @@ git clone https://github.com/pw-02/ezkl-exploration.git -b pw-dev
 cd ezkl-exploration
 # Start an interactive shell
 /bin/bash
+
 #python single-job/launch.py
 # python multi-job/launch.py
