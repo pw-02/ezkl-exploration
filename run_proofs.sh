5ffb0d0b5fbfc30403a47fe7058ca56e9f663786
pw-02 <watters.patrick@outlook.com>
2025-01-23 11:51:29 -0800

    update

--- run_proofs.sh
+++ run_proofs.sh
@@ -5,14 +5,20 @@
 conda activate ezkl
 
 # Execute dispatcher commands for different models
-python distributed_proving/dispatcher.py model=nano_gpt_5_layers_64_embd
-python distributed_proving/dispatcher.py model=nano_gpt_10_layers_64_embd
-python distributed_proving/dispatcher.py model=nano_gpt_15_layers_64_embd
-python distributed_proving/dispatcher.py model=nano_gpt_20_layers_64_embd
-python distributed_proving/dispatcher.py model=nano_gpt_4_layers_112_embd
-python distributed_proving/dispatcher.py model=nano_gpt_4_layers_128_embd
-python distributed_proving/dispatcher.py model=nano_gpt_4_layers_144_embd
-python distributed_proving/dispatcher.py model=nano_gpt_25_layers_64_embd
+
+python distributed_proving/dispatcher.py model=mnist_classifier
+python distributed_proving/dispatcher.py model=mnist_gan
+
+
+
+# python distributed_proving/dispatcher.py model=nano_gpt_5_layers_64_embd
+# python distributed_proving/dispatcher.py model=nano_gpt_10_layers_64_embd
+# python distributed_proving/dispatcher.py model=nano_gpt_15_layers_64_embd
+# python distributed_proving/dispatcher.py model=nano_gpt_20_layers_64_embd
+# python distributed_proving/dispatcher.py model=nano_gpt_4_layers_112_embd
+# python distributed_proving/dispatcher.py model=nano_gpt_4_layers_128_embd
+# python distributed_proving/dispatcher.py model=nano_gpt_4_layers_144_embd
+# python distributed_proving/dispatcher.py model=nano_gpt_25_layers_64_embd
 
 #commands to run the script
 # chmod +x run_dispatcher.sh
