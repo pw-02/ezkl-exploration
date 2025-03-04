857bcef9c195255fa9120d59a526a822ded1ffb4
pw-02 <watters.patrick@outlook.com>
2025-03-03 19:27:26 -0800

    split_group_size: 1 #

--- to_run.sh
+++ to_run.sh
@@ -1,3 +1,4 @@
 
 
-python gen_proof.py model=mnist_classifier model.split_group_size=null #mnist_classifier no splitting
+python gen_proof.py model=mnist_classifier model.split_group_size=null 
+python gen_proof.py model=mnist_gan model.split_group_size=1
