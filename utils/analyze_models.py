b68af0811ca8b46dd97222936385cad9688ff363
pw-02 <watters.patrick@outlook.com>
2025-03-04 16:23:20 -0800

    anlyaze models added

--- utils/analyze_models.py
+++ utils/analyze_models.py
@@ -6,9 +6,21 @@ import csv
 
 mdoels_to_analyze = [
     ('examples/onnx/nanoGPT/nano_gpt_4_layers_64_embd.onnx','nano_gpt_4_layers_64_embd'),
-    ('examples/onnx/nanoGPT/nano_gpt_4_layers_64_embd.onnx','nano_gpt_4_layers_64_embd'),
-    ('examples/onnx/mobile_net/mobilenetv2_050_Opset18.onnx','mobilenetv2_050_Opset18'),
+    ('examples/onnx/nanoGPT/nano_gpt_4_layers_80_embd.onnx','nano_gpt_4_layers_80_embd'),
+    ('examples/onnx/nanoGPT/nano_gpt_4_layers_96_embd.onnx','nano_gpt_4_layers_96_embd'),
+    ('examples/onnx/nanoGPT/nano_gpt_4_layers_112_embd.onnx','nano_gpt_4_layers_112_embd'),
+    ('examples/onnx/nanoGPT/nano_gpt_4_layers_128_embd.onnx','nano_gpt_4_layers_128_embd'),
+    ('examples/onnx/nanoGPT/nano_gpt_4_layers_144_embd.onnx','nano_gpt_4_layers_144_embd'),
+
+    ('examples/onnx/nanoGPT/nano_gpt_5_layers_64_embd.onnx','nano_gpt_5_layers_64_embd'),
     ('examples/onnx/nanoGPT/nano_gpt_10_layers_64_embd.onnx','nano_gpt_10_layers_64_embd'),
+    ('examples/onnx/nanoGPT/nano_gpt_15_layers_64_embd.onnx','nano_gpt_15_layers_64_embd'),
+    ('examples/onnx/nanoGPT/nano_gpt_20_layers_64_embd.onnx','nano_gpt_20_layers_64_embd'),
+    ('examples/onnx/nanoGPT/nano_gpt_25_layers_64_embd.onnx','nano_gpt_25_layers_64_embd'),
+
+    ('examples/onnx/nanoGPT/nano_gpt_12_layers_768_embd.onnx','nano_gpt_12_layers_768_embd'),
+
+    ('examples/onnx/mobile_net/mobilenetv2_050_Opset18.onnx','mobilenetv2_050_Opset18'),
     ('examples/onnx/mobile_net/network.onnx','mobilenet'),
     ('examples/onnx/resnet18/resnet18-v1-7.onnx','resnet18-v1-7.onnx'),
 ]
