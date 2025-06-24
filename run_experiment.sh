4c7644bc7b7872116602df8782e56d810626866c
pw-02 <watters.patrick@outlook.com>
2025-06-24 15:29:37 -0700

    nano_gpt_4_layers_64_embd_split_size_1

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #2,4,6,8,10
-NUM_WORKERS=10    # Change this to however many workers you want
+NUM_WORKERS=2    # Change this to however many workers you want
 
 SESSION=zkexp
 
--- utils/process_reports.py
+++ utils/process_reports.py
@@ -10,13 +10,7 @@ def convert_csv_to_dict(csv_file, start_timestamp = None, end_timestamp = None):
     df = pd.read_csv(csv_file)
 
     # Drop the first row (index 0) as it is warmup
-    df = df.iloc[1:].reset_index(drop=True)
-
-    if 'bill.csv' in csv_file:
-        df['Timestamp'] = pd.to_datetime(df['Timestamp'])
-        # Filter the DataFrame based on the timestamp range
-        # filtered_df = df[(df['Timestamp'] >= start_timestamp) & (df['Timestamp'] <= end_timestamp)]
-        return df.to_dict(orient='list')
+    # df = df.iloc[1:].reset_index(drop=True)
 
     return df.to_dict(orient='list')
 
@@ -68,6 +62,23 @@ def extract_wall_time(log_file):
     else:
         return 0,0
 
+def save_dict_to_csv(data, output_file):
+    with open(output_file, "a", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=data.keys())
+        if f.tell() == 0:
+            writer.writeheader()
+        writer.writerow(data)
+
+def extract_num_workers(report_path):
+    """Extract the number of workers from the report path."""
+    # Assuming the report path contains a pattern like "workers-<num_workers>"
+    match = re.search(r'(\d+)w$', report_path)
+    if match:
+        num_workers = int(match.group(1))
+        return num_workers
+    else:
+        return 1  # Default to 1 if not found
+
 
 
 def process_reports(root_dir):
@@ -75,34 +86,38 @@ def process_reports(root_dir):
     max_memory = find_max_memory_usage(root_dir)
     halo2_perf_file = glob.glob(os.path.join(root_dir, "halo2_perf.csv"))[0]
     wall_time_file = glob.glob(os.path.join(root_dir, "global_job_progress.log"))[0]
-
+    report_path = root_dir.split("reports\\", 1)[1]
     queued_seconds, started_seconds = extract_wall_time(wall_time_file)
-
+    num_workers = extract_num_workers(report_path)
     halo2_job_metrics = convert_csv_to_dict(halo2_perf_file)
 
     # Prepare the report summary
     report_summary = {
-        "agg_circuit_size(n)": sum(halo2_job_metrics.get('agg_circuit_size(n)', 0)),
         "root_directory": root_dir,
+        "num_prover_workers": num_workers,
+        "num_models_to_prove": len(halo2_job_metrics.get('name', 0)),
+        "setup_cached?": "Yes" if sum(halo2_job_metrics.get('vk_time', 0)) <= 0 else "No",
+        "agg_circuit_size(n)": sum(halo2_job_metrics.get('circuit_size(n)', 0)),
         "wall_time_s": started_seconds,
         "max_memory_usage_gb": max_memory,
-        "create_vk_time_s": sum(halo2_job_metrics.get('create_vk_time_s', 0)),
-        "create_pk_time_s": sum(halo2_job_metrics.get('create_pk_time_s', 0)),
-        "read_pk_time_s": sum(halo2_job_metrics.get('read_pk_time_s', 0)),
-        "setup_time_s": sum(halo2_job_metrics.get('create_vk_time_s', 0)) + sum(halo2_job_metrics.get('create_pk_time_s', 0)) + sum(halo2_job_metrics.get('read_pk_time_s', 0)),
-        "proof_time_s": sum(halo2_job_metrics.get('proof_time_s', 0)),
-        "verify_time_s": sum(halo2_job_metrics.get('verify_time_s', 0)),
-
-        
+        "create_vk_time_s": sum(halo2_job_metrics.get('vk_time', 0)),
+        "create_pk_time_s": sum(halo2_job_metrics.get('pk_time', 0)),
+        "read_pk_time_s": sum(halo2_job_metrics.get('read_pk_time', 0)),
+        "setup_time_s": sum(halo2_job_metrics.get('vk_time', 0)) + sum(halo2_job_metrics.get('pk_time', 0)) + sum(halo2_job_metrics.get('read_pk_time', 0)),
+        "proof_time_s": sum(halo2_job_metrics.get('proof_time', 0)),
+        "verify_time_s": sum(halo2_job_metrics.get('verify_time', 0)),
     }
-    
-    # Write the summary to a JSON file
-    summary_file = os.path.join(root_dir, "report_summary.json")
-    with open(summary_file, 'w') as f:
-        import json
-        json.dump(report_summary, f, indent=2)
-    
-    print(f"Report summary written to {summary_file}")
+    save_dict_to_csv(report_summary, "report_summary.csv")
+    # # Write the summary to a tab delimited text file
+    # summary_file = os.path.join(root_dir, "report_summary.txt")
+    # wriet_header = not os.path.exists(summary_file)
+    # with open(summary_file, 'w') as f:
+    #     if wriet_header:
+    #         f.write("Key\tValue\n") 
+    #     for key, value in report_summary.items():
+    #         f.write(f"{key}\t{value}\n")
+   
+    # print(f"Report summary written to {summary_file}")
     print(f"Maximum memory usage found: {max_memory:.2f} GB")
 
 
