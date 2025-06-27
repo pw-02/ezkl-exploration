a70433206cecbb4cb19ef369c25d7e6b73fb0824
pw-02 <watters.patrick@outlook.com>
2025-06-26 21:13:17 -0700

    c1

--- utils/process_reports.py
+++ utils/process_reports.py
@@ -82,46 +82,98 @@ def extract_num_workers(report_path):
 
 
 
-def process_reports(root_dir):
-    """Process all reports in the given root directory."""
-    max_memory = find_max_memory_usage(root_dir)
-    halo2_perf_file = glob.glob(os.path.join(root_dir, "halo2_perf.csv"))[0]
-    wall_time_file = glob.glob(os.path.join(root_dir, "global_job_progress.log"))[0]
-    report_path = root_dir.split("reports\\", 1)[1]
-    queued_seconds, started_seconds = extract_wall_time(wall_time_file)
-    num_workers = extract_num_workers(report_path)
-    halo2_job_metrics = convert_csv_to_dict(halo2_perf_file)
 
+
+def create_summary_report(report_dir):
+    halo2_perf_file = glob.glob(os.path.join(report_dir, "halo2_perf.csv"))[0]
+    halo2_job_metrics = convert_csv_to_dict(halo2_perf_file)
+    # num_workers = extract_num_workers(report_dir.split("reports\\", 1)[1])
+    ec2_instance = report_dir.split("reports\\", 1)[0].split("\\")[-1]
+    
+    if "r6a32xlarge" in report_dir:
+        ec2_instance = "r6a32xlarge"
+        num_instances = 1
+    elif "c5a4xlarge" in report_dir:
+        ec2_instance = "c5a4xlarge"
+        num_instances = halo2_job_metrics.get('global_prover_workers', 0)[0]
+    else:
+        ec2_instance = "unknown"
+    
     # Prepare the report summary
     report_summary = {
-        "reports_path": report_path,
-        "num_prover_workers": num_workers,
-        "num_models_to_prove": len(halo2_job_metrics.get('name', 0)),
-        "setup_cached?": "Yes" if sum(halo2_job_metrics.get('vk_time', 0)) <= 0 else "No",
-        "circuit_size(n)": sum(halo2_job_metrics.get('circuit_size(n)', 0)),
-        "wall_time_s": started_seconds,
+
+        "config": halo2_job_metrics.get('config_name', 'N/A')[0],
+        "job_id": halo2_job_metrics.get('job_id', 'N/A')[0],
+        "ec2_instance": ec2_instance,
+        "num_instances": num_instances,
+        "num_workers": halo2_job_metrics.get('global_prover_workers', 0)[0],
+        #get the last value of the list for the wall time_s
+        "wall_time_s": halo2_job_metrics.get('global_job_time_since_started(s)', 0)[-1],
+        "max_memory_usage_gb": max(halo2_job_metrics.get('max_system_memory(GB)', [])),
+        "avg_cpu_usage": sum(halo2_job_metrics.get('avg_system_cpu(%)', [])) / len(halo2_job_metrics.get('avg_system_cpu(%)', [])) if halo2_job_metrics.get('avg_system_cpu(%)', []) else 0,
         "create_vk_time_s": sum(halo2_job_metrics.get('vk_time', 0)),
         "create_pk_time_s": sum(halo2_job_metrics.get('pk_time', 0)),
         "read_pk_time_s": sum(halo2_job_metrics.get('read_pk_time', 0)),
         "setup_time_s": sum(halo2_job_metrics.get('vk_time', 0)) + sum(halo2_job_metrics.get('pk_time', 0)) + sum(halo2_job_metrics.get('read_pk_time', 0)),
         "proof_time_s": sum(halo2_job_metrics.get('proof_time', 0)),
         "verify_time_s": sum(halo2_job_metrics.get('verify_time', 0)),
-        "ezkl_overhead_time_s": started_seconds - (sum(halo2_job_metrics.get('proof_time', 0)) + sum(halo2_job_metrics.get('vk_time', 0)) + sum(halo2_job_metrics.get('pk_time', 0)) + sum(halo2_job_metrics.get('read_pk_time', 0)) + sum(halo2_job_metrics.get('verify_time', 0))),
-        "max_memory_usage_gb": max_memory,
+        "total_fft_time_s": sum(halo2_job_metrics.get('fft_total_time(s)', 0)),
+        "fft_device": halo2_job_metrics.get('fft_device', 'N/A')[0],
+        "total_msm_time_s": sum(halo2_job_metrics.get('msm_total_time(s)', 0)),
+        "msm_device": halo2_job_metrics.get('msm_device', 'N/A')[0],
 
+        "s3_upload_time_s": sum(halo2_job_metrics.get('s3_upload_time(s)', [])),
+       
     }
     save_dict_to_csv(report_summary, "report_summary.csv")
 
+
+
+
+# def process_reports(root_dir):
+#     """Process all reports in the given root directory."""
+#     max_memory = find_max_memory_usage(root_dir)
+#     halo2_perf_file = glob.glob(os.path.join(root_dir, "halo2_perf.csv"))[0]
+#     wall_time_file = glob.glob(os.path.join(root_dir, "global_job_progress.log"))[0]
+#     report_path = root_dir.split("reports\\", 1)[1]
+#     queued_seconds, started_seconds = extract_wall_time(wall_time_file)
+#     num_workers = extract_num_workers(report_path)
+#     halo2_job_metrics = convert_csv_to_dict(halo2_perf_file)
+
+#     # Prepare the report summary
+#     report_summary = {
+#         "reports_path": report_path,
+#         "num_prover_workers": num_workers,
+#         "num_models_to_prove": len(halo2_job_metrics.get('name', 0)),
+#         "setup_cached?": "Yes" if sum(halo2_job_metrics.get('vk_time', 0)) <= 0 else "No",
+#         "circuit_size(n)": sum(halo2_job_metrics.get('circuit_size(n)', 0)),
+#         "wall_time_s": started_seconds,
+#         "create_vk_time_s": sum(halo2_job_metrics.get('vk_time', 0)),
+#         "create_pk_time_s": sum(halo2_job_metrics.get('pk_time', 0)),
+#         "read_pk_time_s": sum(halo2_job_metrics.get('read_pk_time', 0)),
+#         "setup_time_s": sum(halo2_job_metrics.get('vk_time', 0)) + sum(halo2_job_metrics.get('pk_time', 0)) + sum(halo2_job_metrics.get('read_pk_time', 0)),
+#         "proof_time_s": sum(halo2_job_metrics.get('proof_time', 0)),
+#         "verify_time_s": sum(halo2_job_metrics.get('verify_time', 0)),
+#         "ezkl_overhead_time_s": started_seconds - (sum(halo2_job_metrics.get('proof_time', 0)) + sum(halo2_job_metrics.get('vk_time', 0)) + sum(halo2_job_metrics.get('pk_time', 0)) + sum(halo2_job_metrics.get('read_pk_time', 0)) + sum(halo2_job_metrics.get('verify_time', 0))),
+#         "max_memory_usage_gb": max_memory,
+#         "total_fft_time_s": sum(halo2_job_metrics.get('fft_total_time(s)', 0)),
+#         "fft_device": halo2_job_metrics.get('fft_device', 'N/A')[0],
+#         "total_msm_time_s": sum(halo2_job_metrics.get('msm_total_time(s)', 0)),
+#         "msm_device": halo2_job_metrics.get('msm_device', 'N/A')[0],
+
+#     }
+#     save_dict_to_csv(report_summary, "report_summary.csv")
+
     # print(f"Report summary written to {summary_file}")
-    print(f"Maximum memory usage found: {max_memory:.2f} GB")
+    # print(f"Maximum memory usage found: {max_memory:.2f} GB")
 
 
 if __name__ == "__main__":
 
-    for folder in glob.glob(r"C:\Users\pw\Desktop\reports\nano_gpt_4_layers_64_embd_split_size_1\*"):
+    for folder in glob.glob(r"C:\Users\pw\Desktop\r6a32xlarge\nano_gpt_4_layers_64_embd_split_size_1\*"):
         if os.path.isdir(folder):
             print(f"Processing folder: {folder}")
-            process_reports(folder)
+            create_summary_report(folder)
 
     # folder = r"C:\Users\pw\Desktop\reports\mnist_gan_split_size_1\2025-06-24_19-35-37-4w"  # Change this to your folder
     # process_reports(folder)
