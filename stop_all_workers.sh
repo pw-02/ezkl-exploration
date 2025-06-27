cce5314104395426526a3cff1c474eacfbe8c610
pw-02 <watters.patrick@outlook.com>
2025-06-26 18:44:13 -0700

    host: "34.215.252.40"

--- stop_all_workers.sh
+++ stop_all_workers.sh
@@ -1,2 +1,3 @@
-parallel-ssh -h hosts.txt -l ubuntu -x "-i /home/pw/ezkl-exploration/us-west-2-kp.pem" \
+parallel-ssh -h hosts.txt -l ubuntu \
+-x "-i /home/pw/ezkl-exploration/us-west-2-kp.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" \
 "tmux kill-server || true"
