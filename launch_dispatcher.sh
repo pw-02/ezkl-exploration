5dafa26ddab9a7763f20745a587f3b8c011769ae
pw-02 <watters.patrick@outlook.com>
2025-06-26 18:33:07 -0700

    run_experiment_multi.sh

--- launch_dispatcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SESSION=dispatcher
-tmux new-session -d -s $SESSION
-tmux send-keys -t $SESSION 'conda activate dzkml' C-m
-tmux send-keys -t $SESSION 'cd ezkl-exploration' C-m
-tmux send-keys -t $SESSION 'git pull' C-m         # <-- Added line
-tmux send-keys -t $SESSION 'export PYTHONPATH=.:$PYTHONPATH' C-m
-tmux send-keys -t $SESSION 'python zkInfer/dispatcher.py' C-m
-tmux attach -t $SESSION
---
+++ run_experiment_multi.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#1,2,4,6,8,10
+NUM_WORKERS=${1:-1} # sets NUM_WORKERS to the first argument if provided, otherwise defaults to 1.
+SESSION=zkexp
+
+# Create a new tmux session
+tmux new-session -d -s $SESSION
+
+# Dispatcher in window 0
+tmux rename-window -t $SESSION:0 'dispatcher'
+tmux send-keys -t $SESSION:dispatcher 'conda activate dzkml' C-m
+tmux send-keys -t $SESSION:dispatcher 'cd ezkl-exploration' C-m
+tmux send-keys -t $SESSION:dispatcher 'export PYTHONPATH=.:$PYTHONPATH' C-m
+tmux send-keys -t $SESSION:dispatcher "python zkInfer/dispatcher.py s3_bucket=dzkml dispatcher.num_prover_workers=${NUM_WORKERS}" C-m
+
+# # Start workers in their own windows
+# for i in $(seq 1 $NUM_WORKERS); do
+#     tmux new-window -t $SESSION -n "worker$i"
+#     tmux send-keys -t $SESSION:worker$i 'conda activate dzkml' C-m
+#     tmux send-keys -t $SESSION:worker$i 'cd ezkl-exploration' C-m
+#     tmux send-keys -t $SESSION:worker$i 'export PYTHONPATH=.:$PYTHONPATH' C-m
+#     tmux send-keys -t $SESSION:worker$i 'python zkInfer/worker.py' C-m
+# done
+
+# Optionally: Submit the job in another window
+tmux new-window -t $SESSION -n "submit_job"
+tmux send-keys -t $SESSION:submit_job 'conda activate dzkml' C-m
+tmux send-keys -t $SESSION:submit_job 'cd ezkl-exploration' C-m
+tmux send-keys -t $SESSION:submit_job 'export PYTHONPATH=.:$PYTHONPATH' C-m
+tmux send-keys -t $SESSION:submit_job 'python zkInfer/submit_job.py' C-m
+
+# Attach to the tmux session
+tmux attach -t $SESSION
