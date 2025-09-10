d1dd0ae5eabcb0af1ade64158975a3007eecb1e2
pw-02 <watters.patrick@outlook.com>
2025-09-09 20:05:44 -0700

    distbert

--- examples/onnx/bert/build_from_torch_dist_bert.py
+++ examples/onnx/bert/build_from_torch_dist_bert.py
@@ -1,14 +1,15 @@
 import torch
 import json
 from transformers import BertForQuestionAnswering, BertTokenizer
+from transformers import DistilBertForQuestionAnswering, DistilBertTokenizer
 
 # -------------------------------
 # 1. Load model + tokenizer
 # -------------------------------
-#
-model_name = "google/bert_uncased_L-2_H-128_A-2"  # Tiny BERT
-tokenizer = BertTokenizer.from_pretrained(model_name)
-model = BertForQuestionAnswering.from_pretrained(model_name)
+model_name = "distilbert-base-uncased"  # Tiny BERT
+tokenizer = DistilBertTokenizer.from_pretrained(model_name)
+model = DistilBertForQuestionAnswering.from_pretrained(model_name)
+
 model.eval()
 
 # -------------------------------
@@ -26,19 +27,18 @@ dummy_input = tokenizer(
 
 inputs = (
     dummy_input["input_ids"],
-    dummy_input["attention_mask"],
-    dummy_input["token_type_ids"]
+    dummy_input["attention_mask"]
 )
 
 # -------------------------------
 # 3. Export to ONNX
 # -------------------------------
-onnx_path = "examples/onnx/bert/bert_tiny_squad.onnx"
+onnx_path = "examples/onnx/bert/distilbert_squad.onnx"
 torch.onnx.export(
     model,
     inputs,
     onnx_path,
-    input_names=["input_ids", "attention_mask", "token_type_ids"],
+    input_names=["input_ids", "attention_mask"],
     output_names=["start_logits", "end_logits"],
     opset_version=14,         # EZKL supports 9–18
     do_constant_folding=True,
@@ -52,10 +52,8 @@ print(f"✅ Exported ONNX model: {onnx_path}")
 
 input_list = [
     dummy_input["input_ids"].flatten().tolist(),
-    dummy_input["attention_mask"].flatten().tolist(),
-    dummy_input["token_type_ids"].flatten().tolist()
+    dummy_input["attention_mask"].flatten().tolist()
 ]
 
-with open("bert_input.json", "w") as f:
+with open("examples/onnx/bert/distilbert_input.json", "w") as f:
     json.dump({"input_data": input_list}, f, indent=2)
-
