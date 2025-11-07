52402c0b01ca00d28ecd08ca475b2276ecf5b2a4
pw-02 <watters.patrick@outlook.com>
2025-11-06 16:50:20 -0800

    run_exp

--- run_experiment.sh
+++ run_experiment.sh
@@ -13,7 +13,7 @@ tmux new-session -d -s $SESSION
 
 # Dispatcher in window 0
 tmux rename-window -t $SESSION:0 'dispatcher'
-tmux send-keys -t $SESSION:dispatcher 'conda activate zk-torch' C-m
+tmux send-keys -t $SESSION:dispatcher 'conda activate dzkml' C-m
 tmux send-keys -t $SESSION:dispatcher 'cd ezkl-exploration' C-m
 tmux send-keys -t $SESSION:dispatcher 'export PYTHONPATH=.:$PYTHONPATH' C-m
 tmux send-keys -t $SESSION:dispatcher "python zkInfer/dispatcher.py dispatcher.host=${DISPATCHER_HOST} dispatcher.num_prover_workers=${NUM_WORKERS}" C-m
@@ -21,7 +21,7 @@ tmux send-keys -t $SESSION:dispatcher "python zkInfer/dispatcher.py dispatcher.h
 # Start workers in their own windows
 for i in $(seq 1 $NUM_WORKERS); do
     tmux new-window -t $SESSION -n "worker$i"
-    tmux send-keys -t $SESSION:worker$i 'conda activate zk-torch' C-m
+    tmux send-keys -t $SESSION:worker$i 'conda activate dzkml' C-m
     tmux send-keys -t $SESSION:worker$i 'cd ezkl-exploration' C-m
     tmux send-keys -t $SESSION:worker$i 'export PYTHONPATH=.:$PYTHONPATH' C-m
     tmux send-keys -t $SESSION:worker$i "python zkInfer/worker.py dispatcher.host=${DISPATCHER_HOST}" C-m
@@ -29,7 +29,7 @@ done
 
 # Optionally: Submit the job in another window (using MODEL_NAME and HOST)
 tmux new-window -t $SESSION -n "submit_job"
-tmux send-keys -t $SESSION:submit_job 'conda activate zk-torch' C-m
+tmux send-keys -t $SESSION:submit_job 'conda activate dzkml' C-m
 tmux send-keys -t $SESSION:submit_job 'cd ezkl-exploration' C-m
 tmux send-keys -t $SESSION:submit_job 'export PYTHONPATH=.:$PYTHONPATH' C-m
 tmux send-keys -t $SESSION:submit_job "python zkInfer/submit_job.py model=${MODEL_NAME} dispatcher.host=${DISPATCHER_HOST}" C-m
