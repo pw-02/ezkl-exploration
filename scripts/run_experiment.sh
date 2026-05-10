77524fc8b0e61bf487381f828e6c3f2f3f3b5a4d
pw-02 <watters.patrick@outlook.com>
2026-05-09 18:46:00 -0700

    c1

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -19,7 +19,7 @@ tmux has-session -t "$SESSION" 2>/dev/null && tmux kill-session -t "$SESSION"
 tmux new-session -d -s "$SESSION"
 
 # Common setup command
-SETUP_CMD="cd \"$PROJECT_DIR\" && export PYTHONPATH=.:\"\$PYTHONPATH\""
+SETUP_CMD="export PYTHONPATH=.:\"\$PYTHONPATH\""
 
 # Dispatcher window
 tmux rename-window -t "$SESSION:0" "dispatcher"
